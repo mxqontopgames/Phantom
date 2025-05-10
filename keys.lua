e7519c4f64fecf493f53aa57fef4939753418666
dev422 <133819946+mxqontopgames@users.noreply.github.com>
2025-05-09 23:14:26 -0500

    Update keys

--- keys.lua
+++ keys.lua
@@ -1,4 +1,5 @@
 return {
     ["KEY1CAQE-123123"] = {userId = 123123, active = true},
     ["KEYEK9BW-123"] = {userId = 123, active = true},
+    ["KEY3LPQA-123"] = {userId = 123, active = true},
 }
